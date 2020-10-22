67290f49fcb5173d115897baa1db350a9d48c447
Tsoy Darya <tsoydari@gmail.com>
2020-10-22 18:03:31 +0300

    Lesson 1

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,11 +3,13 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+    lateinit var diceImage: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,11 +18,23 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
             rollDice()
         }
+        diceImage = findViewById(R.id.dice_image)
     }
 
     private fun rollDice() {
-        val textResult:TextView = findViewById(R.id.result_text)
-        textResult.text = resources.getString(R.string.roll).repeat(Random.nextInt(6)+1)
+//        val textResult:TextView = findViewById(R.id.result_text)
+//        textResult.text = resources.getString(R.string.roll).repeat(Random.nextInt(6)+1)
+        val roll_resource = when (Random.nextInt(6)+1) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+
+        }
+        diceImage.setImageResource(roll_resource)
+
 
     }
 }
