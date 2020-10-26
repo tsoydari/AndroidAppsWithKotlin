e4cd44691aefb808e2d432789d1e6c4b3033bc36
Tsoy Darya <tsoydari@gmail.com>
2020-10-26 21:55:51 +0300

    To apply Kotlin Android Extensions

--- AboutMe/app/src/main/java/com/example/aboutme/MainActivity.kt
+++ AboutMe/app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -8,25 +8,22 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        findViewById<Button>(R.id.btnEnterNickname).setOnClickListener {
+        btnEnterNickname.setOnClickListener {
             addNickname(it)
         }
     }
 
     private fun addNickname(view: View) {
-        val editText = findViewById<EditText>(R.id.etEditNickname)
-        val nicknameTextView = findViewById<TextView>(R.id.txtvEnteringNickname)
-
-        nicknameTextView.text = editText.text
-        editText.visibility = View.GONE
+        txtvEnteringNickname.text = etEditNickname.text
+        etEditNickname.visibility = View.GONE
         view.visibility = View.GONE
-        nicknameTextView.visibility = View.VISIBLE
-
+        txtvEnteringNickname.visibility = View.VISIBLE
 
         // Hide the keyboard.
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
