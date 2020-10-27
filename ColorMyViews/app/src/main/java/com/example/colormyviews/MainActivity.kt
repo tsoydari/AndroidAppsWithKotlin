9f48e4b78ecd3fd6aa4f4e7d0987c4616f4338be
Tsoy Darya <tsoydari@gmail.com>
2020-10-27 21:26:16 +0300

    Exercise 27: adding click handers

--- ColorMyViews/app/src/main/java/com/example/colormyviews/MainActivity.kt
+++ ColorMyViews/app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.example.colormyviews
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import android.view.View
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setListeners()
+    }
+
+    private fun setListeners() {
+        val clickableViews: List<View> =
+                listOf(tvBoxOne, tvBoxTwo, tvBoxThree,
+                tvBoxFour, tvBoxFive, tvBox)
+        for (item in clickableViews) {
+            item.setOnClickListener{makeColored(it)}
+        }
+    }
+
+    private fun makeColored(view: View) = when(view.id) {
+        R.id.tvBoxOne -> view.setBackgroundColor(Color.DKGRAY)
+        R.id.tvBoxTwo -> view.setBackgroundColor(Color.GRAY)
+        R.id.tvBoxThree ->
+            view.setBackgroundColor(ContextCompat.getColor(this, android.R.color.holo_blue_light))
+        R.id.tvBoxFour ->
+            view.setBackgroundColor(ContextCompat.getColor(this, android.R.color.holo_green_light))
+        R.id.tvBoxFive ->
+            view.setBackgroundColor(ContextCompat.getColor(this, android.R.color.holo_purple))
+        else -> view.setBackgroundColor(Color.LTGRAY)
     }
 }
