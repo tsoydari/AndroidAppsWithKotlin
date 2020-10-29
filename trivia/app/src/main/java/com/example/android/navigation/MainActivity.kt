7a76af649e4023a3b1923d3c1b8fe2ff90bc113b
Tsoy Darya <tsoydari@gmail.com>
2020-10-29 21:13:31 +0300

    Exersice 9: navigate a button in the title fragment

--- trivia/app/src/main/java/com/example/android/navigation/MainActivity.kt
+++ trivia/app/src/main/java/com/example/android/navigation/MainActivity.kt
@@ -18,13 +18,11 @@ package com.example.android.navigation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
-import com.example.android.navigation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         @Suppress("UNUSED_VARIABLE")
-        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        setContentView(R.layout.activity_main)
     }
 }
--- trivia/app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ trivia/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -5,15 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_title.*
 
-class TitleFragment : Fragment() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_title, container, false)
+class TitleFragment : Fragment(R.layout.fragment_title) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        playButton.setOnClickListener (
+                Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)
+        )
     }
 }
