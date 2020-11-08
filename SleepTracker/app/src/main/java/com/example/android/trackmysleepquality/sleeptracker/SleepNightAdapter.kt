c7f54a2d8202bb8f7a4c13e5942bd0d46448fbd1
Tsoy Darya <tsoydari@gmail.com>
2020-11-09 01:42:08 +0300

    Exercise 4: adding RecyclerView

---
+++ SleepTracker/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -0,0 +1,16 @@
+package com.example.android.trackmysleepquality.sleeptracker
+
+import androidx.recyclerview.widget.RecyclerView
+import com.example.android.trackmysleepquality.TextItemViewHolder
+import com.example.android.trackmysleepquality.database.SleepNight
+
+class SleepNightAdapter: RecyclerView.Adapter<TextItemViewHolder>()  {
+    var data = listOf<SleepNight>()
+
+    override fun getItemCount() = data.size
+
+    override fun onBindViewHolder(holder: TextItemViewHolder, position: Int) {
+        val item = data[position]
+        holder.textView.text = item.sleepQuality.toString()
+    }
+}
