43d63c65b5f879303e45165e47303a0a29700df5
Tsoy Darya <tsoydari@gmail.com>
2020-11-17 00:13:06 +0300

    Exercise 4: adding FloatingActionButton

--- GdgFinder/app/src/main/java/com/example/android/gdgfinder/home/HomeFragment.kt
+++ GdgFinder/app/src/main/java/com/example/android/gdgfinder/home/HomeFragment.kt
@@ -6,8 +6,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
 import com.example.android.gdgfinder.R
+import kotlinx.android.synthetic.main.home_fragment.*
 
 class HomeFragment : Fragment(R.layout.home_fragment) {
 
@@ -16,4 +19,23 @@ class HomeFragment : Fragment(R.layout.home_fragment) {
     }
 
     private val homeViewModel: HomeViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initListenres()
+        initObservers()
+    }
+
+    private fun initListenres() {
+        floatBtnSearch.setOnClickListener{ homeViewModel.onFabClicked() }
+    }
+
+    private fun initObservers() {
+        homeViewModel.navigateToSearch.observe(viewLifecycleOwner, Observer { shouldNavigate ->
+            if (shouldNavigate) {
+                requireView().findNavController().navigate(R.id.action_homeFragment_to_gdgListFragment)
+                homeViewModel.onNavigatedToSearch()
+            }
+        })
+    }
 }
